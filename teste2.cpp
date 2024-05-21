16ab1fe0d0595aae1b1cbe0880b31cb97139b680
Emanuel <gustavo.v.santana@ba.estudante.senai.br>
2024-05-21 20:24:38 -0300

    segundo commit

---
+++ teste2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define TAM 10
+int main(){
+
+    for(int i =0; i < TAM; i++){
+        char nome[20];
+        printf("QUAL SEU NOME? \n");
+        scanf("%d ", &nome );
+
+
+    }
+}
