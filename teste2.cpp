19ea4892cee4e6714cd8614648d4402551eb60d5
Emanuel <gustavo.v.santana@ba.estudante.senai.br>
2024-05-21 20:31:45 -0300

    commit teste

--- teste2.cpp
+++ teste2.cpp
@@ -4,8 +4,15 @@ int main(){
 
     for(int i =0; i < TAM; i++){
         char nome[20];
+        int idade, altura;
         printf("QUAL SEU NOME? \n");
-        scanf("%d ", &nome );
+        scanf("%s ", &nome );
+
+        printf("QUAL SUA IDADE ?");
+        scanf("%d", &idade);
+
+        printf("QUAL SUA ALTURA?");
+        scanf("%d", &altura);
 
 
     }
